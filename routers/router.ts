7afe6d707378385db37848b61744cf397de21683
M0hamed-Gamall <jimmy.mans00ur@gmail.com>
2024-12-11 01:17:25 +0200

    handle post request and insert into database

--- routers/router.ts
+++ routers/router.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express';
 import path from 'path';
+import NoteModel from '../models/noteSchema'
 
 
 const router = Router();
@@ -9,11 +10,20 @@ router.get('/' , (req,res,next)=>{
 })
 
 router.get('/notes' , (req,res,next)=>{
-
+    
 })
 
 router.post('/',async(req,res,next)=>{
-    
+    try{
+        const newNote = new NoteModel({
+            content : req.body.note as string
+        });
+        await newNote.save();
+        res.redirect('/');
+    }catch(err){
+        console.error("can't add new note because : " ,err);
+        res.status(500).send("Internal Server Error");
+    }
 })
 
 router.use((req,res,next)=>{

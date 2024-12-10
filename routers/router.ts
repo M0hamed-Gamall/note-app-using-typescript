2b0fa4b2dd585c06af149b399cf5c1aa667aff83
M0hamed-Gamall <jimmy.mans00ur@gmail.com>
2024-12-10 22:53:50 +0200

    create form page and handle its router

--- routers/router.ts
+++ routers/router.ts
@@ -1,8 +1,9 @@
 import {Router} from 'express';
+import path from 'path'
 const router = Router();
 
 router.get('/' , (req,res,next)=>{
-    
+    res.status(200).sendFile(path.resolve('public', 'home.html'));
 })
 
 router.get('/notes' , (req,res,next)=>{
@@ -10,7 +11,7 @@ router.get('/notes' , (req,res,next)=>{
 })
 
 router.post('/',(req,res,next)=>{
-
+    res.send("post request was sended")
 })
 
 export default router; 
